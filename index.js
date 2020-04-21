92b4fbc16285b7b2f9de99f0b0f2bb8d6fcf85ac
Davi Moraes Ladeira <davimoraesladeira@hotmail.com>
2020-04-20 21:19:40 -0300

    Add

---
+++ index.js
@@ -0,0 +1,8 @@
+function hide(){
+    setTimeout(function(){ document.getElementById("imgbomb").style.visibility="hidden" }, 1000);
+}
+function explosion(){
+    document.getElementsByClassName("img-bomb")[0].setAttribute("src","images/explosion.png")
+    hide()
+}
+
