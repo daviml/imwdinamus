fcae7928ec2dae9cde06d53a17f55efa0bb8364e
Davi Moraes Ladeira <davimoraesladeira@hotmail.com>
2020-04-20 21:33:42 -0300

    Add

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 function hide(){
     setTimeout(function(){ document.getElementById("imgbomb").style.visibility="hidden" }, 1000);
+    
 }
 function explosion(){
     document.getElementsByClassName("img-bomb")[0].setAttribute("src","images/explosion.png")
